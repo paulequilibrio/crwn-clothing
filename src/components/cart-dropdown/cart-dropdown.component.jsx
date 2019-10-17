f2cb24349ab96dcacaf2ba50b777f6488bc8c707
Paulo Alexandre <paulequilibrio@gmail.com>
2019-10-17 17:17:04 -0300

    Add user selectors to improve performance
    Also use createStructuredSelector in all mapStateToProps.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
@@ -18,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = state => ({
-  cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 })
 
 export default connect(mapStateToProps)(CartDropdown)
