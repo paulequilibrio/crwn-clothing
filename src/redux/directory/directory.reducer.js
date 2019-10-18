363195a7e572caf1b3f5e276ece2383c2e17ef4e
Paulo Alexandre <paulequilibrio@gmail.com>
2019-10-18 15:11:51 -0300

    Add shop data to redux store

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,44 +1,7 @@
-import hats from '../../assets/sections/hats.png'
-import jackets from '../../assets/sections/jackets.png'
-import sneakers from '../../assets/sections/sneakers.png'
-import womens from '../../assets/sections/womens.png'
-import men from '../../assets/sections/men.png'
+import DIRECTORY_DATA from './directory.data'
 
 const INITIAL_STATE = {
-  sections: [
-    {
-      title: 'hats',
-      imageUrl: hats,
-      id: 1,
-      linkUrl: 'shop/hats'
-    },
-    {
-      title: 'jackets',
-      imageUrl: jackets,
-      id: 2,
-      linkUrl: 'shop/jackets'
-    },
-    {
-      title: 'sneakers',
-      imageUrl: sneakers,
-      id: 3,
-      linkUrl: 'shop/sneakers'
-    },
-    {
-      title: 'womens',
-      imageUrl: womens,
-      size: 'large',
-      id: 4,
-      linkUrl: 'shop/womens'
-    },
-    {
-      title: 'mens',
-      imageUrl: men,
-      size: 'large',
-      id: 5,
-      linkUrl: 'shop/mens'
-    }
-  ]
+  sections: DIRECTORY_DATA
 }
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
