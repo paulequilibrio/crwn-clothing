22facb61ba59b4e3d7912534e3f8b97912e841e6
Paulo Alexandre <paulequilibrio@gmail.com>
2019-11-01 07:31:36 -0300

    Add stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+import Logo from '../../assets/crown.svg'
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100
+  const publishableKey = 'pk_test_RicOJFWgOtPlwpIowPaOz6n700Gg4LA5Xx'
+
+  const onToken = token => {
+    console.log(token)
+    alert('Payment Successful')
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image={Logo}
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton
