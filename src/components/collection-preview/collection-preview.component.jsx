396a9e0cd6954f62e2689d515e36bf78190f6bad
Paulo Alexandre <paulequilibrio@gmail.com>
2020-06-06 08:31:09 -0300

    Add withSpinner HOC

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview.styles.scss'
 
+import { Link } from 'react-router-dom'
+
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <Link to={`/shop/${title.toLowerCase()}`}>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+    </Link>
     <div className='preview'>
       {
         items
