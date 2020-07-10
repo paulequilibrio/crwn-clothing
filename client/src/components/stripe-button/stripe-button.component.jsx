9095668ce8ac6a9b2b9f7bfc58cfe28b21c2b1a6
Paulo Alexandre <paulequilibrio@gmail.com>
2020-07-10 16:42:08 -0300

    Add backend express server handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 import Logo from '../../assets/crown.svg'
+import axios from 'redaxios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
@@ -8,7 +9,21 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = token => {
     console.log(token)
-    window.alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      console.log(response)
+      window.alert('Payment successful')
+    }).catch(error => {
+      console.error('Payment error:', JSON.parse(error))
+      window.alert(`There was an issue with your payment.
+      Please sure you use the provided credit card.`)
+    })
   }
 
   return (
